6046c298d135e15fd0215ea396dc8bc28e41fde3
Abel Gheller <abelgheller@gmail.com>
2025-10-16 20:42:13 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-  return <div><h1> Hello World </h1> </div>;
+  return (
+    <div>
+      <h1>
+        Rosas são vermelhas,
+        Violetas são azuis,
+        Código é poesia,
+        E Next.js nos conduz.
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
